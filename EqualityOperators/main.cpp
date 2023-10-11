c2512bbb917293b559ec0d5f9e48335a42786497
AndreD4 <andremai629@gmail.com>
2023-10-12 07:50:19 +0900

    set up

--- EqualityOperators/main.cpp
+++ EqualityOperators/main.cpp
@@ -5,11 +5,20 @@ using namespace std;
 int main() 
 {
     
+    bool equal_result {false};
+    bool not_equal_result {false};
+
+    int num1{}, num2{};
+    
+//    cout << boolalpha;   //Will display true or false instead of 1/0 for booleans
     
     
-    
-    
-    
+    cout << "Enter two integers seprarated by space: ";
+    cin >> num1 >> num2;
+    equal_result = (num1 == num2);
+    not_equal_result = (num1 != num2);
+    cout << "Comparision result (equal): " << equal_result << endl;
+    cout << "Comparision result (not equal): "<<  not_equal_result << endl;
     
     
     
