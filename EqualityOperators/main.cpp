49ba3347d1f796d4886821febe85911a5399f017
AndreD4 <andremai629@gmail.com>
2023-10-12 09:34:37 +0900

    Example

--- EqualityOperators/main.cpp
+++ EqualityOperators/main.cpp
@@ -10,18 +10,24 @@ int main()
 
     int num1{}, num2{};
     
-//    cout << boolalpha;   //Will display true or false instead of 1/0 for booleans
+    cout << boolalpha;   //Will display true or false instead of 1/0 for booleans
     
     
+//    cout << "Enter two integers seprarated by space: ";
+//    cin >> num1 >> num2;
+//    equal_result = (num1 == num2);
+//    not_equal_result = (num1 != num2);
+//    cout << "Comparision result (equal): " << equal_result << endl;
+//    cout << "Comparision result (not equal): "<<  not_equal_result << endl;
+    
+    
+    char char1{}, char2{};
     cout << "Enter two integers seprarated by space: ";
-    cin >> num1 >> num2;
-    equal_result = (num1 == num2);
-    not_equal_result = (num1 != num2);
+    cin >> char1 >> char2;
+    equal_result = (char1 == char2);
+    not_equal_result = (char1 != char2);
     cout << "Comparision result (equal): " << equal_result << endl;
     cout << "Comparision result (not equal): "<<  not_equal_result << endl;
-    
-    
-    
     
     
     
