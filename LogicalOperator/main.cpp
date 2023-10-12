523bd30c732cd624299073de97b4fc88b5683a55
AndreD4 <andremai629@gmail.com>
2023-10-12 12:04:43 +0900

    Using AND && and OR ||

--- LogicalOperator/main.cpp
+++ LogicalOperator/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() 
 {
-    int num {};
-    const int lower {10};
-    const int upper {20};
+//    int num {};
+//    const int lower {10};
+//    const int upper {20};
     
     cout << boolalpha;
     
@@ -31,15 +31,33 @@ int main()
     
     // Determine if an integer is exactly on the boundary
     // assume lower < upper
-    cout << "\nEnter an integer - the bounds are " << lower << " and " << upper << " : ";
-    cin >> num;
+//    cout << "\nEnter an integer - the bounds are " << lower << " and " << upper << " : ";
+//    cin >> num;
+//    
+//    bool on_bounds {false};
+//    on_bounds = (num == lower || num == upper);
+//    cout << num << " is on one of the bounds which are " << lower << " and " << upper << " : " << on_bounds << endl;
     
-    bool on_bounds {false};
-    on_bounds = (num == lower || num == upper);
-    cout << num << " is on one of the bounds which are " << lower << " and " << upper << " : " << on_bounds << endl;
+    // Determine is you need to wear a coat based on temperature and wind speed
+    bool wear_coat {false};
+    double temperature {};
+    int wind_speed {};
     
+    const int wind_speed_for_coat {25}; // wind over the value requires a coat
+    const int temperature_for_coat {45}; // temperature below this value requires a coat
     
+    //Require a coat if either wind is too high OR temperature is too low
+    cout << "\nEnter the current temperature in (F):";
+    cin >> temperature;
+    cout << "Enter windspeed in (mph): ";
+    cin >> wind_speed;
     
+    wear_coat = (temperature < temperature_for_coat || wind_speed > wind_speed_for_coat);
+    cout << "Do you need to wear a coat OR?" << wear_coat << endl;
+    
+    //Require a coat if BOTH the windspeed is too high AND too low
+    wear_coat = (temperature < temperature_for_coat && wind_speed > wind_speed_for_coat);
+    cout << "Do you need to wear a coat using AND? " << wear_coat << endl;
     
     
     
