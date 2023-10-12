3c1d6c868fc84d7b1d7dfa8598be19b38dc8bd01
AndreD4 <andremai629@gmail.com>
2023-10-12 15:36:05 +0900

    Challenge instruction

---
+++ Challenge/main.cpp
@@ -0,0 +1,30 @@
+
+/*
+    Write a program that asks user to enter the following:
+    An integer representing the number cents
+    
+    
+    You may assume that the number of cents entered is greater than or equal zero
+    
+    the program should then display how to provide that change to the user
+    
+    in the US:
+        1 dollar is 100 cents
+        1 quarters is 25 censt 
+        1 dime is 10 cents 
+        1 nickel 5 cents 
+        and 
+        1 penny is 1 cent
+    
+    Here is a sample run:
+    
+    Enter an amount in cents: 92
+    
+    You can provide this change as follow:
+    dollars : 0
+    quarters : 3 
+    dimes : 1
+    nickels : 1
+    pennis : 2
+    
+    
